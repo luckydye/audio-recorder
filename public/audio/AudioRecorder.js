100ce24c86ccada2cb14e82b5b6fffa9c83e4fb1
Tim Havlicek <tim.h4vlicek@gmail.com>
2021-05-20 15:12:59 +0200

    refactor for AudioRecorder

--- public/audio/AudioRecorder.js
+++ public/audio/AudioRecorder.js
@@ -1,56 +1,167 @@
+function makeAudioBuffer(audioContext, chunks) {
+    const numberOfChannels = chunks[0].length;
+    const chunkSize = chunks[0][0].length;
+
+    const buffer = audioContext.createBuffer(numberOfChannels, chunks.length * chunkSize, audioContext.sampleRate);
+
+    for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+        const channelBuffer = buffer.getChannelData(channel);
+        for(let chunk in chunks) {
+            for(let i = 0; i < chunkSize; i++) {
+                channelBuffer[i + (chunk * chunkSize)] = chunks[chunk][channel][i];
+            }
+        }
+    }
+    return buffer;
+}
+
+function getLiveBuffer(audioChunks) {
+    const chunkBuffer = [[], []];
+
+    for(let chunk of audioChunks) {
+        const channel1 = chunk[0];
+        const channel2 = chunk[1];
+
+        for(let sample of channel1) {
+            chunkBuffer[0].push(sample);
+        }
+        for(let sample of channel2) {
+            chunkBuffer[1].push(sample);
+        }
+    }
+
+    return chunkBuffer;
+}
+
+function drawAudioBuffer(buffer, duration, sampleRate) {
+
+    const channelCount = buffer.length;
+
+    const scale = 100; // 100 pixel equals 1second of audio
+
+    const canvas = document.createElement('canvas');
+    canvas.height = 200;
+
+    canvas.style.background = "grey";
+    canvas.style.borderTop = "10px solid #eee";
+
+    const ctxt = canvas.getContext("2d");
+    canvas.ctxt = ctxt;
+
+    const draw = () => {
+        const dataLength = sampleRate * duration;
+        canvas.width = (dataLength / (sampleRate / scale));
+
+        for(let channel = 0; channel < channelCount; channel++) {
+            const data = buffer[channel];
+    
+            const height = canvas.height / channelCount;
+            const yOffset = height * channel;
+            const y = yOffset + (height/2);
+    
+            ctxt.fillRect(0, height, canvas.width, 1);
+            ctxt.moveTo(0, y);
+    
+            for(let i = 0; i < data.length; i+=scale) {
+                const x = i / (sampleRate / scale);
+                ctxt.lineTo(x, y + data[i] * height);
+            }
+    
+            ctxt.strokeStyle = "white";
+            ctxt.stroke();
+        }
+    }
+
+    draw();
+
+    return {
+        canvas,
+        update: (newBuffer, currDuration) => {
+            buffer = newBuffer;
+            duration = currDuration;
+            draw();
+        }
+    };
+}
+
 export class AudioRecorder {
 
-    constructor(stream) {
-        this.recorder = new MediaRecorder(stream);
-        this._initAnalyser(stream);
+    constructor(audioContext) {
+        this.context = audioContext;
+        this.currentRecTime = 0;
+        this.recording = false;
+        this.audioChunks = [];
+        this.audioProcessor = new AudioWorkletNode(this.context, 'audio-processor');
+        this.startRecordTs = 0;
+        this.currClip = null;
 
-        this.recorder.ondataavailable = e => {
-            this.onData(e.data);
+        this.audioProcessor.port.onmessage = msg => {
+            if (this.recording) {
+                this.audioChunks.push(msg.data[0]);
+                this.currentRecTime = (this.context.currentTime - this.startRecordTs);
+            }
         }
 
-        this.recorder.onstop = e => {
-            console.log('Stoped recording');
-        }
+        setInterval(() => {
+            if(this.currClip) {
+                const chunkBuffer = getLiveBuffer(this.audioChunks);
+                this.currClip.update(chunkBuffer, this.currentRecTime);
+            }
+        }, 1000 / 30);
     }
 
-    onData() {
-        // data callback
+    setInput(input) {
+        this.input = input;
+        this.input.connect(this.audioProcessor);
     }
 
-    getData() {
-        if(this.recorder.state !== "inactive") {
-            return this.recorder.requestData();
-        }
-        return null;
+    clearInput() {
+        this.input.disconnect();
+        this.input = null;
+    }
+
+    connect(output) {
+        this.audioProcessor.connect(output);
+    }
+
+    disconnect() {
+        this.audioProcessor.disconnect();
     }
 
     startRecord() {
-        this.recorder.start();
+        this.audioChunks = [];
+        this.currentRecTime = 0;
+        this.startRecordTs = this.context.currentTime;
+        this.recording = true;
+
+        const chunkBuffer = getLiveBuffer(this.audioChunks);
+        this.currClip = drawAudioBuffer(chunkBuffer, this.currentRecTime, this.context.sampleRate);
+        tracksElement.appendChild(this.currClip.canvas);
     }
 
     stopRecord() {
-        this.recorder.stop();
-    }
+        this.recording = false;
 
-    _initAnalyser(stream) {
-        const context = new AudioContext();
-        const analyser = context.createAnalyser();
+        const buffer = makeAudioBuffer(this.context, this.audioChunks);
 
-        const source = context.createMediaStreamSource(stream);
-        source.connect(analyser);
+        const chunkBuffer = getLiveBuffer(this.audioChunks);
+        this.currClip.update(chunkBuffer, this.currentRecTime);
+        this.currClip = null;
 
-        analyser.fftSize = 2048;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
+        this.playLastBuffer = () => {
+            const sourceNode = this.context.createBufferSource();
+            sourceNode.buffer = buffer;
+            sourceNode.connect(this.context.destination);
+            sourceNode.start();
 
-        setInterval(() => {
-            if(this.recorder.state === "recording") {
-                analyser.getByteTimeDomainData(dataArray);
-                const chunk = [...dataArray];
-                const values = chunk.map(v => v / 128.0);
-                
+            sourceNode.onended = () => {
+                sourceNode.disconnect();
             }
-        }, 1000 / 2);
+        }
     }
 
+    playLastBuffer() {
+
+    }
+    
 }
--- public/main.js
+++ public/main.js
@@ -15,7 +15,6 @@ function monitorStream(stream, name, contianer) {
 async function main() {
     // setup audiocontext
     await audioContext.audioWorklet.addModule('./audio/audio-processor.js');
-    const audioProcessor = new AudioWorkletNode(audioContext, 'audio-processor');
 
     // init routing
     const source = new AudioSource(audioContext);
@@ -43,162 +42,22 @@ async function main() {
     const outputStream = channel.getOutputStream();
     const ouputNode = channel.getOutputNode();
 
-    // const clip = new AudioClip();
-
-    ouputNode.connect(audioProcessor);
-
-    let recording = false;
-    let startRecordTs = 0;
-    let endRecordTs = 0;
-    let currentRecTime = 0;
-
-    let audioChunks = [];
-
-    audioProcessor.port.onmessage = msg => {
-        const data = msg.data;
-
-        if (recording) {
-            audioChunks.push(data[0]);
-            currentRecTime = (audioContext.currentTime - startRecordTs);
-        }
-    }
-
-    function makeAudioBuffer(chunks) {
-        const numberOfChannels = chunks[0].length;
-        const chunkSize = chunks[0][0].length;
-
-        const buffer = audioContext.createBuffer(numberOfChannels, chunks.length * chunkSize, audioContext.sampleRate);
-
-        for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
-            const channelBuffer = buffer.getChannelData(channel);
-            for(let chunk in chunks) {
-                for(let i = 0; i < chunkSize; i++) {
-                    channelBuffer[i + (chunk * chunkSize)] = chunks[chunk][channel][i];
-                }
-            }
-        }
-        return buffer;
-    }
-
-    function getLiveBuffer(audioChunks) {
-        const chunkBuffer = [[], []];
-
-        for(let chunk of audioChunks) {
-            const channel1 = chunk[0];
-            const channel2 = chunk[1];
-
-            for(let sample of channel1) {
-                chunkBuffer[0].push(sample);
-            }
-            for(let sample of channel2) {
-                chunkBuffer[1].push(sample);
-            }
-        }
-
-        return chunkBuffer;
-    }
-
-    // ui
-    let currClip = null;
-
-    setInterval(() => {
-        if(currClip) {
-            const chunkBuffer = getLiveBuffer(audioChunks);
-            currClip.update(chunkBuffer, currentRecTime);
-        }
-    }, 1000 / 30);
+    const recorder = new AudioRecorder(audioContext);
+    recorder.setInput(ouputNode);
 
     const ui = makeUi();
     ui.onStart = () => {
-        audioChunks = [];
-        currentRecTime = 0;
-        startRecordTs = audioContext.currentTime;
-        recording = true;
-
-        const chunkBuffer = getLiveBuffer(audioChunks);
-        currClip = drawAudioBuffer(chunkBuffer, currentRecTime, audioContext.sampleRate);
-        tracksElement.appendChild(currClip.canvas);
+        recorder.startRecord();
     }
     ui.onStop = () => {
-        recording = false;
-
-        const buffer = makeAudioBuffer(audioChunks);
-
-        const chunkBuffer = getLiveBuffer(audioChunks);
-        currClip.update(chunkBuffer, currentRecTime);
-        currClip = null;
-
-        let playStartTime = Date.now();
-        window.playLastBuffer = () => {
-            playStartTime = Date.now();
-            
-            const sourceNode = audioContext.createBufferSource();
-            sourceNode.buffer = buffer;
-            sourceNode.connect(audioContext.destination);
-            sourceNode.start();
-
-            sourceNode.onended = () => {
-                sourceNode.disconnect();
-            }
-        }
+        recorder.stopRecord();
     }
     ui.onPlay = () => {
-        window.playLastBuffer();
+        recorder.playLastBuffer();
     }
 
     // monitor
     monitorStream(outputStream, "Input", headerElement);
-}
-
-function drawAudioBuffer(buffer, duration, sampleRate) {
-
-    const channelCount = buffer.length;
-
-    const scale = 100; // 100 pixel equals 1second of audio
-
-    const canvas = document.createElement('canvas');
-    canvas.height = 200;
-
-    canvas.style.background = "grey";
-    canvas.style.borderTop = "10px solid #eee";
-
-    const ctxt = canvas.getContext("2d");
-    canvas.ctxt = ctxt;
-
-    const draw = () => {
-        const dataLength = sampleRate * duration;
-        canvas.width = (dataLength / (sampleRate / scale));
-
-        for(let channel = 0; channel < channelCount; channel++) {
-            const data = buffer[channel];
-    
-            const height = canvas.height / channelCount;
-            const yOffset = height * channel;
-            const y = yOffset + (height/2);
-    
-            ctxt.fillRect(0, height, canvas.width, 1);
-            ctxt.moveTo(0, y);
-    
-            for(let i = 0; i < data.length; i+=scale) {
-                const x = i / (sampleRate / scale);
-                ctxt.lineTo(x, y + data[i] * height);
-            }
-    
-            ctxt.strokeStyle = "white";
-            ctxt.stroke();
-        }
-    }
-
-    draw();
-
-    return {
-        canvas,
-        update: (newBuffer, currDuration) => {
-            buffer = newBuffer;
-            duration = currDuration;
-            draw();
-        }
-    };
 }
 
 function makeUi() {
